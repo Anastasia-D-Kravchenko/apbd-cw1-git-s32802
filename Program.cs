35ef3d59ce0de516274222a15c5f2dc1a0a12161
Anastasia-D-Kravchenko <s32802@pjwstk.edu.pl>
2026-03-10 13:33:20 +0100

    Implement basic input handling

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using apbd_cw1_git_s32802;
+
+Console.WriteLine("Welcome to the Statistics App!");
+Console.WriteLine("Please enter a series of numbers separated by spaces:");
+
+string? input = Console.ReadLine();
+string[] numbers = input?.Split(' ') ?? Array.Empty<string>();
+
+Console.WriteLine($"You entered {numbers.Length} potential numbers.");
