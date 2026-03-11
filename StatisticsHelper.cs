12d31cccc0602485544922ff4718acc8134f462b
Anastasia-D-Kravchenko <s32802@pjwstk.edu.pl>
2026-03-11 23:36:50 +0100

    Added CalculateAverage method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -2,7 +2,16 @@ namespace apbd_cw1_git_s32802;
 
 public class StatisticsHelper
 {
-    
-    // Class will store math logic in future
-    
+    public static double CalculateAverage(int[] numbers) {
+        if (numbers == null || numbers.Length == 0) {
+            return 0;
+        }
+
+        int sum = 0;
+        foreach (var n in numbers) {
+            sum += n;
+        }
+
+        return (double)sum / numbers.Length;
+    }
 }
